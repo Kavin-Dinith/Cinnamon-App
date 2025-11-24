bf5cdacacb41913917815c11112bc8cb641c3100
Kavin-Dinith <kwasantha709@gmail.com>
2025-11-24 09:53:34 +0530

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<ProductsPage />} />
-      </Routes>
+      {/* Header */}
+      <Navbar />
+
+      {/* Main Content */}
+      <div className="pt-16"> {/* optional: padding to offset fixed navbar */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductsPage />} />
+          {/* Add more routes here */}
+        </Routes>
+      </div>
+
+      {/* Footer */}
+      <Footer />
     </Router>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,15 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutCinnamon from "../components/AboutCinnamon";
 import Usage from "../components/Usage";
 import Benefits from "../components/Benefits";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <Hero />
       <AboutCinnamon />
       <Usage />
       <Benefits />
-      <Footer />
     </div>
   );
 }
